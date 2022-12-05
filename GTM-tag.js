739ec6df296058d315c1f9677beed68bb16b5b9a
Jason Spanomanolis <jason@jslytics.com>
2022-12-05 22:30:32 +0000

    Update GTM-tag.js

--- GTM-tag.js
+++ GTM-tag.js
@@ -1,16 +1,15 @@
 <script>
-  // Include the library file
-  var script = document.createElement("script");
-  script.src = "https://euphonious-sprite-03dbfc.netlify.app/lib.js";
-  document.head.appendChild(script);
+  // Import the TabGrab object from the library file
+  var TabGrab = require("https://euphonious-sprite-03dbfc.netlify.app/lib.js");
 
   // Initialize the TabGrab service after the library has been loaded
-  script.onload = function() {
+  window.addEventListener("load", function() {
     var faviconUrl = "https://upmostly.com/wp-content/uploads/android-chrome-192x192-2.png";
     var pageTitle = "(1) Don't forget your tutorial";
     var controlPopulation = 50;
     var timeout = 1500;
 
+    // Initialize the service using the TabGrab.init function
     TabGrab.init(faviconUrl, pageTitle, controlPopulation, timeout);
-  }
+  });
 </script>
